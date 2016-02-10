1a12bf1ad3ad1318e2888874f5a4f4870c813227
NoFatAsiansPlz <leonevets@gmail.com>
2016-02-09 21:02:34 -0500

    Update

--- AutoChat2/AutoChat/Properties/AssemblyInfo.cs
+++ AutoChat2/AutoChat/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutoChat")]
+[assembly: AssemblyProduct("AutoChat2")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
